038d8d6536bdf97e9138b56c77032e582ca4db9d
CalebAllard <allard.caleb@gmail.com>
2019-11-08 16:38:22 -0500

    init commit

---
+++ my-app/src/coponents/header/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from './nav';
+
+export default function Header(props){
+
+    return (
+        <header>
+            <img src="#" alt="logo" />
+            <Nav />
+        </header>
+    );
+
+}
---
+++ my-app/src/coponents/header/nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Nav(){
+
+
+    return (
+
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Log In</a>
+            
+        </nav>
+
+    );
+
+
+}
